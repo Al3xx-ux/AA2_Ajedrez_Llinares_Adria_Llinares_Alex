8dcccf40f364578bc626281e12816fee0d038636
Alex Llinares <alex.llinares@enti.cat>
2025-03-26 12:06:46 +0100

    2n comit

--- AA2_Ajedrez_Llinares_Adria_Llinares_Alex/AA2_Ajedrez_Llinares/AA2_Ajedrez_Llinares/main.cpp
+++ AA2_Ajedrez_Llinares_Adria_Llinares_Alex/AA2_Ajedrez_Llinares/AA2_Ajedrez_Llinares/main.cpp
@@ -9,13 +9,12 @@ int main() {
     iniciarTablero(tableroAjedrez);
 
     do {
-        system("cls");  // Limpia la pantalla
+        system("cls");  
         mostrarTablero(tableroAjedrez);
 
         Position posicion;
         bool tiene_pieza = false;
 
-        // Solicitar las coordenadas de la pieza
         do {
             std::cout << "Elige una pieza" << std::endl;
             std::cout << "X: ";
@@ -25,7 +24,7 @@ int main() {
 
             if (posicion.x < 1 || posicion.x > TAMANYO_TABLERO ||
                 posicion.y < 1 || posicion.y > TAMANYO_TABLERO) {
-                std::cout << "Input inválido, intenta de nuevo.\n";  // Corregido el acento
+                std::cout << "Input inválido, intenta de nuevo." << std::endl ;  
             }
             else {
                 tiene_pieza = true;
@@ -33,15 +32,13 @@ int main() {
 
         } while (!tiene_pieza);
 
-        // Ajustar las coordenadas
+     
         posicion.x--;
         posicion.y = TAMANYO_TABLERO - posicion.y;
 
-        // Preguntar si el jugador quiere salir del juego
-        std::cout << "¿Quieres salir del juego? (1 = Sí, 0 = No): ";  // Corregido el acento
-        std::cin >> gameOver;
+ 
 
     } while (!gameOver);
 
-    return 0;
+
 }
